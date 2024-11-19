348410d0bdcd17c048046ebf94d40838739fba93
nguyentanphatt <bloodomega97@gmail.com>
2024-11-19 21:58:38 +0700

    navbar

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,26 @@
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                    ✨7.5M seed round raised
+                </div>
+                <h1>Impactful design, created effortlessly</h1>
+                <p>
+                    Design tools shouldnt slow you down. Layers combines
+                    powerful features with d intuitive interface that keep you
+                    in your creative flow.
+                </p>
+                <form action="">
+                    <input
+                        type="email"
+                        name=""
+                        id=""
+                        placeholder="Enter your email"
+                    />
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+        </section>
+    );
 }
