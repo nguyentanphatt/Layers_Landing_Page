d1d367731502fec678df00403326af4d0627d8ba
nguyentanphatt <bloodomega97@gmail.com>
2024-11-26 19:09:55 +0700

    completed design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import CallToAction from "@/sections/CallToAction";
+import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
+import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
@@ -13,5 +16,8 @@ export default function Home() {
             <Introduction />
             <Features />
             <Integrations />
+            <Faqs />
+            <CallToAction />
+            <Footer />
         </>
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,5 @@
+import logoImg from '@/assets/images/logo.svg'
+import Image from 'next/image';
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +7,22 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return <section className='py-16'>
+        <div className="container">
+            <div className='flex flex-col md:flex-row md:justify-between items-center gap-6'>
+                <div>
+                    <Image src={logoImg} alt='Footer Logo' />
+                </div>
+                <div>
+                    <nav className='flex gap-6'>
+                        {footerLinks.map((footer) => (
+                          <a href={footer.href}
+                            className='text-white/50 text-sm'
+                          >{footer.label}</a>  
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
 }
